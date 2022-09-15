f2c6bfbf5b4b3f2fe97f72341edd808ad2a9fedd
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-15 12:55:58 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "FIN-Botz" //namabot kalian
+global.ownername= "Alfin" //nama kalian
+global.myweb ="https://linktr.ee/fin222" //bebas asal jan hapus
+global.youtube = "https://youtube.com/channel/UCCZFKpk85uGzGdGk9kUZUIA" //bebas asal jan hapus
+global.myweb2 = "https://telegra.ph/Donate-09-15-2" //bebas
+global.email = "udrehan8@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["6285342106390","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6282261065144'] //premium user
+global.owner = ["6282261065144","6282261065144"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6282261065144" // nomor wa kalian
+global.ownernomerr = "+6282261065144" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.thumbGif = 'https://i.postimg.cc/FsSHms85/menu.png'
+global.packname = '© FIN-Md' //sticker wm ubah
+global.author = 'Di Buat Oleh ALFIN' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
